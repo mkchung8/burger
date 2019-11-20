32bd51d7040e6d8ce99d3344785d7d0e3290fb94
Miyoung Chung <mkchung.8@gmail.com>
2019-11-20 15:44:55 -0700

    initialized SQL queries and installed dependencies

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Sound & The Curry Burger", false); 
+INSERT INTO burgers (burger_name, devoured) VALUES ("If Looks Could Kale Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Don't Get Creme Fraiche With Me", false); 
+INSERT INTO burgers (burger_name, devoured) VALUES ("Onion-tended Consequences Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("She's a Super-Leek Burger", false); 
