fe8408ec5d8e826abc01ebc9f041558f4792dfab
Miyoung Chung <mkchung.8@gmail.com>
2019-11-21 14:01:26 -0700

    added controller functions

--- controllers/burger-controller.js
+++ controllers/burger-controller.js
@@ -2,18 +2,32 @@ var express = require("express");
 
 var router = express.Router(); 
 
-var burger = require("../models/burger"); 
+var burger = require("../models/burger");
 
 router.get("/", function (req, res){
-
+    burger.selectAll(function(data){
+        var hbsObject = {
+            burgers: data
+        }; 
+        console.log(hbsObject); 
+        res.render("index", hbsObject); 
+    }); 
 }); 
 
 router.post("/api/bugers", function(req,res){
-
+    burger.insertOne(req.body.burger_name, function(result){
+        res.redirect("/"); 
+    });
 });
 
 router.put("/api/burgers/:id", function (req, res){
-
+    let id = req.params.id; 
+    burger.updateOne(id, function(result){
+        if (result.changedRows === 0) {
+            return res.status(404).end(); 
+        }
+        res.redirect("/"); 
+    }); 
 }); 
 
 module.exports = router; 
